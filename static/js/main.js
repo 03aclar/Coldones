daa0832d090297e831d42386d4c11d14b73e558c
Ashley <ashley.clarke1991@gmail.com>
2022-06-16 12:36:06 +0100

    Flask_week2

---
+++ static/js/main.js
@@ -0,0 +1,18 @@
+function funcExample () {
+    console.log("hello world");
+};
+
+let my_name = "Ash" 
+    
+const my_last_name = "Clarke"
+
+if (my_name === "Ash") {
+console.log(`hello, ${my_name}!`)
+} else {
+    console.log("you're not Ash")
+}
+
+function reallygoodfunction (num1, num2) {
+    let result = num1 + num2
+    return result
+}
